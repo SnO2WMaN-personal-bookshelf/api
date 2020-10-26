9968f3f6b25999c983d96e514eb51659bf47993d
SnO2WMaN <sno2wman.official@gmail.com>
2020-10-26 15:30:13 +0900

    chore: :whale: サンプルデータの読み込み修正

--- docker/mongo/initdb/2-mongo-init.sh
+++ docker/mongo/initdb/2-mongo-init.sh
@@ -3,3 +3,8 @@ mongoimport \
   --db bookshelf --collection books \
   --drop \
   --file /sample/books.json --jsonArray
+mongoimport \
+  -u sample -p password \
+  --db bookshelf --collection series \
+  --drop \
+  --file /sample/series.json --jsonArray
