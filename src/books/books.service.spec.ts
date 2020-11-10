5bf76176f7eb7fc98b2b65cb5c825bfe41f9f22e
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-10 17:37:07 +0900

    test: :sparkles: bookの書影部分取得についてのテスト

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,7 +1,10 @@
+import {HttpModule, HttpService} from '@nestjs/common';
 import {getModelToken, MongooseModule} from '@nestjs/mongoose';
 import {Test, TestingModule} from '@nestjs/testing';
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import {Model} from 'mongoose';
+import {of} from 'rxjs';
+import booksConfig from './books.config';
 import {BooksService} from './books.service';
 import {Book, BookSchema} from './schema/book.schema';
 
@@ -9,7 +12,9 @@ describe('BookService', () => {
   let mongoServer: MongoMemoryServer;
 
   let bookModel: Model<Book>;
+
   let bookService: BooksService;
+  let httpService: HttpService;
 
   beforeAll(async () => {
     mongoServer = new MongoMemoryServer();
@@ -22,12 +27,23 @@ describe('BookService', () => {
           useFactory: async () => ({uri: await mongoServer.getUri()}),
         }),
         MongooseModule.forFeature([{name: Book.name, schema: BookSchema}]),
+        HttpModule,
       ],
-      providers: [BooksService],
+      providers: [
+        {
+          provide: booksConfig.KEY,
+          useValue: {
+            bookcoverServerUrl: 'example.dev',
+          },
+        },
+        BooksService,
+      ],
     }).compile();
 
     bookModel = module.get<Model<Book>>(getModelToken(Book.name));
+
     bookService = module.get<BooksService>(BooksService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   afterEach(async () => {
@@ -40,5 +56,49 @@ describe('BookService', () => {
 
   it('should be defined', () => {
     expect(bookService).toBeDefined();
+  });
+
+  describe('bookcover', () => {
+    it('書影サーバから404が返ってくる場合', async () => {
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        of({
+          data: undefined,
+          status: 404,
+          statusText: 'Not found book',
+          headers: {},
+          config: {},
+        }),
+      );
+
+      const book = await bookModel.create({
+        _id: '1',
+        title: 'title',
+        authors: [],
+      });
+
+      const actual = await bookService.bookcover(book);
+      expect(actual).toBeNull();
+    });
+
+    it('書影サーバと接続不能', async () => {
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        of({
+          data: undefined,
+          status: 500,
+          statusText: 'connect ECONNREFUSED',
+          headers: {},
+          config: {},
+        }),
+      );
+
+      const book = await bookModel.create({
+        _id: '1',
+        title: 'title',
+        authors: [],
+      });
+
+      const actual = await bookService.bookcover(book);
+      expect(actual).toBeNull();
+    });
   });
 });
