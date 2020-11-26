c00681266a590aff1913747daaa3a84d30d1f1f0
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-26 19:53:36 +0900

    build: GraphQLスキーマ生成スクリプトをビルドに含めないように

--- src/scripts/create-graphql-schema.ts
+++ src/scripts/create-graphql-schema.ts
@@ -8,13 +8,13 @@ import {writeFile} from 'fs';
 import {printSchema} from 'graphql';
 import * as path from 'path';
 import {promisify} from 'util';
-import {AuthorsResolver} from './authors/authors.resolver';
-import {BooksResolver} from './books/books.resolver';
-import {BookshelfRecordsResolver} from './bookshelf-records/bookshelf-records.resolver';
-import {BookshelvesResolver} from './bookshelves/bookshelves.resolver';
-import {SearchResolver} from './search/search.resolver';
-import {SeriesRecordResolver, SeriesResolver} from './series/series.resolver';
-import {UsersResolver} from './users/users.resolver';
+import {AuthorsResolver} from '../authors/authors.resolver';
+import {BooksResolver} from '../books/books.resolver';
+import {BookshelfRecordsResolver} from '../bookshelf-records/bookshelf-records.resolver';
+import {BookshelvesResolver} from '../bookshelves/bookshelves.resolver';
+import {SearchResolver} from '../search/search.resolver';
+import {SeriesRecordResolver, SeriesResolver} from '../series/series.resolver';
+import {UsersResolver} from '../users/users.resolver';
 
 const schemaPath = path.resolve(process.cwd(), 'schema.graphql');
 
