a61f84e2293937e4ca75cfe2d85d1e268de46b24
SnO2WMaN <sno2wman.official@gmail.com>
2020-11-26 12:27:02 +0900

    test: :sparkles: 取得したユーザーの本棚が全て異なることをテスト

--- src/users/test/medium/users.service.test.ts
+++ src/users/test/medium/users.service.test.ts
@@ -12,7 +12,7 @@ import {TypeORMConfigService} from '../../../typeorm/typeorm.service';
 import {User} from '../../entity/user.entity';
 import {UsersService} from '../../users.service';
 
-describe('UsersService with connected DB', () => {
+describe('UsersService(実際にDBに接続)', () => {
   let module: TestingModule;
 
   let connection: Connection;
@@ -104,6 +104,48 @@ describe('UsersService with connected DB', () => {
       await expect(
         usersService.createUser('auth0:2', {name: 'test_user_1'}),
       ).rejects.toThrow(`User name test_user_1 is already used`);
+    });
+  });
+
+  describe('getUserById()', () => {
+    describe('createUserでユーザーを生成する', () => {
+      let newUser: User;
+      beforeEach(async () => {
+        newUser = await usersService.createUser('auth0:1', {
+          name: 'test_user',
+          displayName: 'Test Name',
+          picture: 'https://example.com/test_user',
+        });
+      });
+
+      it('ユーザーを取得してプロパティを検証', async () => {
+        const actual = (await usersService.getUserById(newUser.id))!;
+        expect(actual).toBeDefined();
+
+        expect(actual).toHaveProperty('id', newUser.id);
+
+        expect(actual).toHaveProperty('name', newUser.name);
+        expect(actual).toHaveProperty('displayName', newUser.displayName);
+        expect(actual).toHaveProperty('picture', newUser.picture);
+
+        expect(actual).toHaveProperty('readBooks');
+        expect(actual.readBooks).toBeDefined();
+
+        expect(actual).toHaveProperty('readingBooks');
+        expect(actual.readingBooks).toBeDefined();
+
+        expect(actual).toHaveProperty('wishBooks');
+        expect(actual.wishBooks).toBeDefined();
+      });
+
+      it('ユーザーの読んだ本，呼んでいる本，読みたい本のIDがすべて異なる', async () => {
+        const actual = (await usersService.getUserById(newUser.id))!;
+
+        expect(actual.readBooks.id).not.toBe(actual.readingBooks.id);
+        expect(actual.readBooks.id).not.toBe(actual.wishBooks.id);
+
+        expect(actual.readingBooks.id).not.toBe(actual.wishBooks.id);
+      });
     });
   });
 });
